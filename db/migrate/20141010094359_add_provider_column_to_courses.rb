24224185448caebcf94517a1dec55a53f94de13b
Kim Fransman <kim.fransman@gmail.com>
2014-10-10 12:42:27 +0200

    updated migration to not updatee data, seed is updated

--- db/migrate/20141010094359_add_provider_column_to_courses.rb
+++ db/migrate/20141010094359_add_provider_column_to_courses.rb
@@ -1,9 +1,5 @@
 class AddProviderColumnToCourses < ActiveRecord::Migration
   def change
     add_column :courses, :provider, :string
-    Course.update_all("provider = smartgolfa")
-
-
-    Course.where(name: "Kyssinge Golf").first.update_attribute(:provider, 'both')
   end
 end
